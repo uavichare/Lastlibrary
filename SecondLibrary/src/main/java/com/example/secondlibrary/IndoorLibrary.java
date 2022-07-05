83746e053c39423226950df6b4343b9299bbcf70
dilipgupta123-123 <GDIlip10#$%>
2022-07-05 18:30:43 +0530

    added new

---
+++ SecondLibrary/src/main/java/com/example/secondlibrary/IndoorLibrary.java
@@ -0,0 +1,37 @@
+package com.example.secondlibrary;
+
+import android.content.Context;
+
+import com.example.ToasterLibrary;
+
+
+public class IndoorLibrary {
+
+
+    public static void s(Context c, String message){
+
+        ToasterLibrary.s(c,message);
+
+       // Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+    }
+
+/*
+    public static void callNavigation(Activity v,int  id){
+      //  Navigation.findNavController(v,R.id.nav_host_fragment).navigate(R.id.home_dashboard);
+
+    }
+
+    public static void getNavFragment(Context c, int  id)
+    {
+        Intent intent = null;
+        try {
+            intent = new Intent(c,Class.forName("com.example.fragmentTest") );
+            c.startActivity(intent);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+*/
+
+}
